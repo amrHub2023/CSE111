7227a89d2550b5db6dc2713842308d695443b9f9
AMORENO <amrmoreno07@gmail.com>
2025-01-17 19:31:08 -0300

    add get verb

--- sentences.py
+++ sentences.py
@@ -57,3 +57,34 @@ def get_noun(quantity):
 # Randomly choose and return a determiner
   word = random.choice(words)
   return word
+
+def get_verb(quantity, tense):
+  """Return a randomly chosen verb. If tense is "past",
+  this function will return one of these ten verbs:
+      "drank", "ate", "grew", "laughed", "thought", "ran", "slept", "talked", "walked", "wrote"
+      
+  If tense is "present" and quantity is 1, this
+  function will return one of these ten verbs:
+      "drinks", "eats", "grows", "laughs", "thinks","runs", "sleeps", "talks", "walks", "writes"
+      
+  If tense is "present" and quantity is NOT 1,
+  this function will return one of these ten verbs:
+      "drink", "eat", "grow", "laugh", "think","run", "sleep", "talk", "walk", "write"
+      
+  If tense is "future", this function will return one of
+  these ten verbs:
+      "will drink", "will eat", "will grow", "will laugh","will think", "will run", "will sleep", "will talk","will walk", "will write"
+      
+  Parameters
+      quantity: an integer that determines if the
+          returned verb is single or plural.
+      tense: a string that determines the verb conjugation,
+          either "past", "present" or "future".
+  Return: a randomly chosen verb.
+  """
+  if tense.lowercase() == "past":
+      verbs = ["drank", "ate", "grew", "laughed", "thought", "ran", "slept", "talked", "walked", "wrote"]
+  elif tense.lowercase() == "past"::
+      verbs = ["birds", "boys", "cars", "cats", "children","dogs", "girls", "men", "rabbits", "women"]
+Randomly choose and return a determiner
+word = random.choice(words)
