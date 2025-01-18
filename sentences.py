8aa05b5a914b3b48beaf40e954ffa89b1aa0c594
AMORENO <amrmoreno07@gmail.com>
2025-01-18 11:04:30 -0300

    add get proposition

--- sentences.py
+++ sentences.py
@@ -8,10 +8,38 @@ def main():
         user_res=get_valid_option()
         if user_res.lower() == "n":
             break
+        
+        
+def get_prepositional_phrase(quantity):
+  """Build and return a prepositional phrase composed
+  of three words: a preposition, a determiner, and a
+  noun by calling the get_preposition, get_determiner,
+  and get_noun functions.
+  Parameter
+      quantity: an integer that determines if the
+          determiner and noun in the prepositional
+          phrase returned from this function should
+          be single or pluaral.
+  Return: a prepositional phrase.
+  """       
+  preposition=get_preposition() 
+  determiner=get_determiner(quantity)
+  noun=get_noun(quantity)
+  
 def get_preposition():
-    print()
-def get_prepositional_phrase():        
-    print()
+    """Return a randomly chosen preposition
+    from this list of prepositions:
+     "about", "above", "across", "after", "along","around", "at", "before", "behind", "below",
+     "beyond", "by", "despite", "except", "for","from", "in", "into", "near", "of",
+     "off", "on", "onto", "out", "over","past", "to", "under", "with", "without"
+      Return: a randomly chosen preposition.
+    """
+    preposition = ["about", "above", "across", "after", "along","around", "at", "before", "behind", "below",
+    "beyond", "by", "despite", "except", "for","from", "in", "into", "near", "of",
+    "off", "on", "onto", "out", "over","past", "to", "under", "with", "without"]
+    # Randomly choose and return a determiner. 
+    print(f"The random preposition is: ",preposition)
+    return random.choice(preposition)
         
 def get_valid_option():#get a valid number from the user    
         while True:
